d173c63039f7d9a3bca80b4ddb70007ec025f09f
soroush4208 <soroush.r80@gmail.com>
2024-07-06 18:46:12 +0330

    Initial project infrastructure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,45 @@
+import "@/i18n";
 import "@/styles/globals.css";
+import { ShopTheme } from "@/themes/ShopTheme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <ThemeProvider theme={ShopTheme}>
+      <CssBaseline />
+      <QueryClientProvider client={queryClient}>
+        {getLayout(
+          <>
+            <ToastContainer />
+            <Component {...pageProps} />
+          </>
+        )}
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
 }
