03e15a6ef4c8102081da58f9bb66a4acf941fd24
soroush4208 <soroush.r80@gmail.com>
2024-07-07 20:45:57 +0330

    feat : Create locales Language Persian and English

---
+++ src/i18n/locales-en/en.ts
@@ -0,0 +1,53 @@
+const en = {
+  header: {
+    name: "Electroland",
+    profile: "profile",
+    Messages: "Messages",
+    Notifications: "Notifications",
+    login: "Login",
+    logout: "Logout",
+    wishList: "WishList",
+    Orders: "Orders",
+    Payments: "Payments",
+    home: "Home",
+    products: "Products",
+    aboutUs: "About Us",
+    contactUs: "Contact Us",
+  },
+  footer: {
+    Offers: "STAY UPTO DATE ABOUT OUR LATEST OFFERS",
+    Description:
+      "We have products that are right for you and that you will be proud to work with. From women to men.",
+    company: {
+      title: "COMPANY",
+      About: "About",
+      features: "Features",
+      works: "Works",
+      career: "Career",
+    },
+    help: {
+      title: "HELP",
+      customer_support: "Customer Support",
+      delivery_details: "Delivery Details",
+      terms_conditions: "Terms & Conditions",
+      privacy_policy: "Privacy Policy",
+    },
+    faq: {
+      title: "FAQ",
+      account: "Account",
+      manege_deliveries: "Manage Deliveries",
+      orders: "Orders",
+      payments: "Payment",
+    },
+    resources: {
+      title: "RESOURCES",
+      free_eBook: "Free eBook",
+      development_tutorial: "Development Tutorial",
+      how_to_blog: "How To Blog",
+      youtube_playlist: "Youtube Playlist",
+    },
+    Additional_explanations:"ElectroLand © 2000-2023, All Rights Reserved"
+  },
+};
+
+export default en;
