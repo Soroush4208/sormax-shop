2fc571a4fcbfe1dda58de055018cabee3bd17cc4
soroush4208 <soroush.r80@gmail.com>
2024-07-10 10:26:51 +0330

    ref : Refactor code in ErrorPageNotFound

--- src/components/error/ErrorNotFound.tsx
+++ src/components/error/ErrorNotFound.tsx
@@ -30,7 +30,11 @@ function ErrorPageNotFound() {
         component="img"
         src={Error_Image.src}
         alt="error image"
-        sx={{ fontFamily: ShopTheme.typography.fontFamily ,width:"30%"}}
+        sx={{
+          fontFamily: ShopTheme.typography.fontFamily,
+          width: "30%",
+          minWidth: "400px",
+        }}
       />
       <Typography sx={{ fontSize: "40px", color: "#e5472f" }}>
         {t("error.title")}
@@ -51,6 +55,7 @@ function ErrorPageNotFound() {
           fontWeight: "bold",
           fontSize: "20px",
           my: "40px",
+          mt: "150px",
           ":hover": { background: "#1976d2", color: "white" },
           borderRadius: "8px",
         }}
