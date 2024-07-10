da27724e3b9b86a0ca5b73ffa8c43b809624997e
soroush4208 <soroush.r80@gmail.com>
2024-07-10 10:13:07 +0330

    feat : Create error page

--- src/components/error/ErrorNotFound.tsx
+++ src/components/error/ErrorNotFound.tsx
@@ -1,5 +1,64 @@
-function ErrorNotFound() {
-  return <div>ErrorNotFound</div>;
+import Error_Image from "@/assets/gif/error/gif error Shop Logo.gif";
+import { ShopTheme } from "@/themes/ShopTheme";
+import { Box, Button, Typography } from "@mui/material";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+
+function ErrorPageNotFound() {
+  const router = useRouter();
+  const { t } = useTranslation();
+  useEffect(() => {
+    if (router.asPath !== "/404") {
+      router.replace("/404");
+    }
+  }, [router]);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#f9f5e0",
+      }}
+    >
+      <Box
+        component="img"
+        src={Error_Image.src}
+        alt="error image"
+        sx={{ fontFamily: ShopTheme.typography.fontFamily ,width:"30%"}}
+      />
+      <Typography sx={{ fontSize: "40px", color: "#e5472f" }}>
+        {t("error.title")}
+      </Typography>
+      <Typography
+        sx={{
+          fontSize: "20px",
+          width: "50%",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        {t("error.description")}
+      </Typography>
+      <Button
+        variant="outlined"
+        sx={{
+          fontWeight: "bold",
+          fontSize: "20px",
+          my: "40px",
+          ":hover": { background: "#1976d2", color: "white" },
+          borderRadius: "8px",
+        }}
+      >
+        <Link href={"/"}>{t("error.button")}</Link>
+      </Button>
+    </Box>
+  );
 }
 
-export default ErrorNotFound;
+export default ErrorPageNotFound;
