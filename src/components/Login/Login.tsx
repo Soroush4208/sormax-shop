92aac21a562b826f32eac51d7fa576ef615f921b
soroush4208 <soroush.r80@gmail.com>
2024-07-14 19:20:35 +0330

    feat : Create Login Page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,12 +1,40 @@
-import SignIn from "./sign-in/SignIn";
-import SingUp from "./sign-up/SingUp";
+import TabLogin from "@/components/Login/tabs/TabLogin";
+import Box from "@mui/material/Box";
 
 function Login() {
   return (
-    <div>
-      <SingUp />
-      <SignIn />
-    </div>
+    <Box
+      sx={{
+        backgroundImage:
+          'linear-gradient(to bottom right, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url("/background.avif")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          width: { xs: "90%", sm: "70%", md: "60%" },
+          maxWidth: 750,
+          backgroundColor: "rgba(255, 255, 255, 0.9)",
+          border: "1px solid rgba(255, 255, 255, 0.5)",
+          minHeight: 550,
+          borderRadius: 4,
+          boxShadow: "0 8px 30px rgba(0, 0, 0, 0.3)",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backdropFilter: "blur(15px)",
+          p: 4,
+        }}
+      >
+        <TabLogin />
+      </Box>
+    </Box>
   );
 }
 
