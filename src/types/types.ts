025bc7c4fe8fbc43978f75e40ec9fc1bd2ac740c
soroush4208 <soroush.r80@gmail.com>
2024-07-15 04:32:32 +0330

    feat : Create file for Types

---
+++ src/types/types.ts
@@ -0,0 +1,16 @@
+export type IProps = {};
+
+export type IPropsSignIn = {
+  username: string;
+  password: string;
+};
+
+export type UserTypeSignUp = {
+  firstname: string;
+  lastname: string;
+  username: string;
+  password: string;
+  phoneNumber: string;
+  address: string;
+  termsAccepted: boolean;
+};
