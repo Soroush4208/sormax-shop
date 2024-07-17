ddbf8e450286c40a160ffa4ed9d60a454a2c6e74
soroush4208 <soroush.r80@gmail.com>
2024-07-17 17:10:27 +0330

    ref : Refactor code hn theme

--- src/themes/ShopTheme.tsx
+++ src/themes/ShopTheme.tsx
@@ -1,21 +1,40 @@
-import { createTheme } from "@mui/material";
+import useStore from "@/store/useStore";
+import { createTheme, Theme, ThemeProvider } from "@mui/material";
 import { Vazirmatn } from "@next/font/google";
+import { ReactNode, useEffect } from "react";
 
 const vazirmatn = Vazirmatn({ subsets: ["latin"] });
 
-export const ShopTheme = createTheme({
-  typography: {
-    fontFamily: `${vazirmatn.style.fontFamily}, Vazirmatn, sans-serif`,
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        body: {
-          fontFamily: `${vazirmatn.style.fontFamily}, Vazirmatn, sans-serif`,
-          direction: "rtl",
+interface DynamicThemeProviderProps {
+  children: ReactNode;
+}
+
+const DynamicThemeProvider = ({ children }: DynamicThemeProviderProps) => {
+  const language = useStore((state) => state.language);
+  const direction = useStore((state) => state.direction);
+
+  useEffect(() => {
+    document.documentElement.dir = direction;
+  }, [direction]);
+
+  const theme: Theme = createTheme({
+    typography: {
+      fontFamily: `${vazirmatn.style.fontFamily}, Vazirmatn, sans-serif`,
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: {
+          body: {
+            fontFamily: `${vazirmatn.style.fontFamily}, Vazirmatn, sans-serif`,
+            direction: direction,
+          },
         },
       },
     },
-  },
-  direction: "rtl",
-});
+    direction: direction,
+  });
+
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+export default DynamicThemeProvider;
