8efaee4ef9f4b4e4e506d9432c02014a3cbf752e
soroush4208 <soroush.r80@gmail.com>
2024-07-18 09:53:44 +0330

    feat : Change type product and users

--- src/types/types.ts
+++ src/types/types.ts
@@ -14,3 +14,39 @@ export type UserTypeSignUp = {
   address: string;
   termsAccepted: boolean;
 };
+
+interface Category {
+  name: string;
+}
+interface Subcategory {
+  name: string;
+}
+
+export type IProduct = {
+  _id: string;
+  category: Category;
+  subcategory: Subcategory;
+  name: string;
+  price: number;
+  quantity: number;
+  brand: string;
+  description: string;
+  thumbnail?: string;
+  images: string[];
+  createdAt: string;
+  updatedAt?: string;
+  slugname?: string;
+  __v?: number;
+};
+
+export type IUserType = {
+  _id: string;
+  firstname: string;
+  lastname: string;
+  username: string;
+  phoneNumber: string;
+  address: string;
+  role?: string;
+  createdAt?: string;
+  updatedAt?: string;
+};
