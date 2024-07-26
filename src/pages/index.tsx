af91e5e4addd7ab1c351fa094374d6372d9d4800
soroush4208 <soroush.r80@gmail.com>
2024-07-26 09:14:21 +0330

    feat : Create SSR in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
 import Home from "@/components/home/components/Home";
 import Layout from "@/layout/Layout";
 import { NextPageWithLayout } from "@/pages/_app";
+import axios from "@/utils/axiosConfig";
+import { GetServerSideProps } from "next";
 
-const HomePage: NextPageWithLayout = () => {
-  return <Home />;
+const HomePage: NextPageWithLayout = ({ data }: any) => {
+  return <Home data={data} />;
 };
 
 HomePage.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const response = await axios.get(
+    "/products?sort=-createdAt&page=1&limit=all"
+  );
+  const data = response.data.data.products;
+  console.log(data);
+  return {
+    props: {
+      data,
+    },
+  };
 };
 
 export default HomePage;
